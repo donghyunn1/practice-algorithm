28c2001ed9a8176870c8bedb6e94c0e65e6bd418
donghyunn1 <156062658+donghyunn1@users.noreply.github.com>
2024-09-24 15:28:05 +0900

    [Bronze II] Title: 그릇, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/7567. 그릇/그릇.py
@@ -0,0 +1,13 @@
+lst = list(str(input()))
+
+ch = lst[0]
+sum = 0
+for i in range(len(lst)):
+    if i == 0:
+        sum += 10
+    elif lst[i] == lst[i - 1]:
+        sum += 5
+    else:
+        sum += 10
+
+print(sum)
