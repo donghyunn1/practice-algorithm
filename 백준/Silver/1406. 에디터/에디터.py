07861ced64467cd7d818adee92746ea1085910c9
donghyunn1 <156062658+donghyunn1@users.noreply.github.com>
2024-10-07 15:15:47 +0900

    [Silver II] Title: 에디터, Time: 264 ms, Memory: 42128 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,22 @@
+import sys
+
+left = list(input())
+right = []
+
+for _ in range(int(input())):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'L' and left:
+        right.append(left.pop())
+    
+    elif command[0] == 'D' and right:
+        left.append(right.pop())
+
+    elif command[0] == 'B' and left:
+        left.pop()
+
+    elif command[0] == 'P':
+        left.append(command[1])
+
+answer = left + right[::-1]
+print(''.join(answer))
