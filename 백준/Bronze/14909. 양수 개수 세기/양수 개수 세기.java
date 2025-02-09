0bd7224966b56ef0e1c2f3b193b18cf41d1ee1e4
donghyunn1 <156062658+donghyunn1@users.noreply.github.com>
2025-02-09 23:53:31 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 1576 ms, Memory: 311228 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {        
+        Scanner sc = new Scanner(System.in);
+        int cnt =0;
+        while(sc.hasNextInt()) {
+            int a = sc.nextInt();
+            if(a >0)
+                cnt++;
+            
+        }
+        System.out.print(cnt);
+        sc.close();
+    }
+}
