f98f8c19b37cd1697512be702f9f327c87318269
DongHyeon Yang <156062658+donghyunn1@users.noreply.github.com>
2025-12-29 13:28:11 +0900

    [Silver III] Title: 퇴사, Time: 172 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,25 +1,27 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] d = new int[n + 2];
-        int[] t = new int[n + 2];
-        int[] p = new int[n + 2];
+        int[] dp = new int[n + 2];
+        int[] t = new int[n + 1];
+        int[] p = new int[n + 1];
+        
         for (int i = 1; i <= n; i++) {
             t[i] = sc.nextInt();
             p[i] = sc.nextInt();
         }
-        for (int i = n; i > 0; i--) {
+        
+        for (int i = n; i >= 1; i--) {
             if (i + t[i] > n + 1) {
-                d[i] = d[i + 1];
+                dp[i] = dp[i + 1];
             } else {
-                d[i] = Math.max(d[i + 1], p[i] + d[i + t[i]]);
+                dp[i] = Math.max(dp[i + 1], dp[i + t[i]] + p[i]);
             }
         }
-        System.out.println(d[1]);
-    }
+
+        System.out.println(dp[1]);
+    } 
 }
